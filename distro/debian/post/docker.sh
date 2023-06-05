c9858fa1adfe80b8cc8925be23f03e0ed6456062
Chris Murray <cmurray@927.technology>
2023-06-04 20:59:33 -0500

    added post docker install

---
+++ distro/debian/post/docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#dependancies
+apt-get update
+apt-get install ca-certificates curl gnupg
+
+#gpg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+#repo
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#install
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#autostart service
+systemctl enable docker
