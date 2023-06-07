57d22407e927866fbba13d9419ef217fbdf8449f
Chris Murray <cmurray@927.technology>
2023-06-06 21:32:25 -0500

    add git for player

--- distro/debian/xinit/player.sh
+++ distro/debian/xinit/player.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#prerequesites
+apt install -y git
+
+#set xinitrc script
 cat << 'EOF_xinitrc' > /home/player/.xinitrc
 #!/bin/bash
 /usr/bin/matchbox-window-manager & /usr/local/bin/attract
@@ -8,6 +12,7 @@ EOF_xinitrc
 chown root:player /home/player/.xinitrc
 chmod 750 /home/player/.xinitrc
 
+#set bashrc script
 rm -f /home/player/.bashrc
 
 cat << 'EOF_bashrc' > /home/player/.bashrc
@@ -18,4 +23,11 @@ EOF_bashrc
 chown root:player /home/player/.bashrc
 chmod 750 /home/player/.bashrc
 
+#add player permission groups for xserver
 usermod -aG audio,docker,tty,video player
+
+#get git setup for arcade
+mkdir -p /home/player/git
+cd /home/player/git
+git clone https://github.com/927technology/arcade.git
+ls -s ./git/arcade/config/.attract /home/player/.attract
